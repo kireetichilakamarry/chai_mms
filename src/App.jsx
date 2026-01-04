3fe23796df72da8c4cb87d5a0c9ee20cba171479
Kireeti Chilakamarry <desichilz123@gmail.com>
2026-01-04 13:07:07 -0800

    Slightly Tweaked the webpage's aesthetics

--- src/App.jsx
+++ src/App.jsx
@@ -60,14 +60,21 @@ function App() {
         <h1>Content Health Accuracy Index (CHAI) for Health-Related Social Media Content </h1>
       </div>
       <div>
-        <h2> About The Tool </h2>
+        <h2 class="headers"> About The Tool </h2>
         <p> Accuracy and quality of medical content on social media is of significant concern due to variability, accessibility, and lack of oversight. The CHAI is a tool that providers and researchers can use to appraise health related content on social media. </p>
       </div>
+      
+      <hr />
+
       <div>
-        <h2> Why Use </h2>
+        <h2 class="headers"> Why Use </h2>
         <p> Health information on social media is prevalent but highly variable in quality and oversight is limited. Although research on misinformation is growing, there is no widely adopted, objective tool to assess accuracy and quality, particularly for short-form videos. CHAI fills this gap by offering a structured, evidence-informed scoring approach that supports research, education, and public health efforts to curb misinformation. CHAI was initially developed to study Crohn’s disease–related content and has since been generalized to any medical topic and adapted for modern social media formats. Also can be used for vetting posts to share with patients, trainees, or the public (e.g., clinic resource lists, teaching sessions). </p>
       </div>
+
+      <hr />
+
       <div>
+        <h2 class="headers">Survey Questions </h2>
         <h3> Please answer the following questions. </h3>
         {questionBank.map((question) => (
           /* Passing in options based on the content type. */
@@ -91,11 +98,14 @@ function App() {
           />
         ))}
       </div>
-      
+
+      <br />
+      <hr />
+
       {/* Total score and interpretation. */}
       <div>
+        <h2 class="headers">Total Score & Interpretation </h2>
         <div>
-          <h2>Total Score & Interpretation </h2>
           {score == 1 ? (
             <h3>Total Score: {score} Point</h3>
           ): (
@@ -109,35 +119,39 @@ function App() {
           <ul>
             <li>
               <p>
-                0-3 = poor overall quality
+                0-3 = Poor Overall Quality
               </p>
             </li>
             <li>
               <p>
-                4-7 = moderate quality
+                4-7 = Moderate Quality
               </p>
             </li>
             <li>
               <p>
-                8-10 = high quality
+                8-10 = High Quality
               </p>
             </li>
           </ul>
         </div>
       </div>
 
+      <hr />
+
       {/* Creator Insights */}
       <div>
-        <h2>Evidence </h2>
+        <h2 class="headers">Evidence </h2>
         <p>
           Madabhushi, Sankirth MD*; Patel, Ami DO, MPH; Liu, Andrew MD; Tse-Kang, Samantha PhD; Rupawala, Abbas MD. S1852 Reel Talk: Evaluating the Informational Landscape of Crohn’s Disease Related Instagram Videos Through a Novel Scoring System. The American Journal of Gastroenterology 120(10S2):p S398, October 2025. | DOI: 10.14309/01.ajg.0001134868.05536.4b. 
           <a href="https://journals.lww.com/ajg/fulltext/2025/10002/s1852_reel_talk__evaluating_the_informational.1853.aspx ">https://journals.lww.com/ajg/fulltext/2025/10002/s1852_reel_talk__evaluating_the_informational.1853.aspx </a>
         </p>
       </div>
 
+      <hr />
+
       {/* Disclaimer Section */}
       <div>
-        <h2>CHAI Score Disclaimer </h2>
+        <h2 class="headers">CHAI Score Disclaimer </h2>
         <p>
           The Content Health Accuracy Index (CHAI) is an educational tool for evaluating health-related social media content. Scores reflect the reviewer’s judgment based on information available at the time and may be subjective or incomplete. CHAI scores are not medical advice and should not be used as the sole basis for diagnosis, treatment, or clinical decision-making. For personal medical guidance, consult a qualified clinician.
         </p>
